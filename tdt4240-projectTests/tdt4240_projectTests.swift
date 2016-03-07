d20d50ae362bd2bef36863001b9c3acee163e045
Kristoffer Larsen <kristoffer@larsen.so>
2016-03-07 14:15:29 +0100

    Initial Commit

---
+++ tdt4240-projectTests/tdt4240_projectTests.swift
@@ -0,0 +1,36 @@
+//
+//  tdt4240_projectTests.swift
+//  tdt4240-projectTests
+//
+//  Created by Kristoffer Larsen on 07.03.2016.
+//  Copyright © 2016 group-1. All rights reserved.
+//
+
+import XCTest
+@testable import tdt4240_project
+
+class tdt4240_projectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
