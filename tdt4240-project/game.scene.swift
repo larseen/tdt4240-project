41a8c90f5191d41e742fc015c4bd9144c79a8969
Kristoffer Larsen <kristoffer@larsen.so>
2016-04-08 19:24:05 +0200

    #10 added mallet for testing

--- tdt4240-project/game.scene.swift
+++ tdt4240-project/game.scene.swift
@@ -7,10 +7,24 @@ class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         
         let game = Game.instance
-      //  let board = game.getBoard(CGRectGetMaxY(self.frame), height: CGRectGetMaxX(self.frame))
+        let board = game.getBoard(self.frame.size.width, height: self.frame.size.height)
         let puck = game.getPuck(self.frame.size.width, height: self.frame.size.height)
-      //  self.addChild(board)
-        self.addChild(settingsLabel)
+        let mallet = game.getMallet(self.frame.size.width, height: self.frame.size.height)
+        board.size = self.frame.size
         self.addChild(puck)
+        self.addChild(board)
+        self.addChild(mallet)
+        
     }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)  {
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            if(nodeAtPoint(location).name == "mallet"){
+                let touchedNode = nodeAtPoint(location)
+                touchedNode.position = location
+            }
+        }
+    }
+    
 }
