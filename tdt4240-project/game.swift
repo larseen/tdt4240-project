1a4b9ddb0dd27ab27332e7088f4e1a3e6661f98f
Kristoffer Larsen <kristoffer@larsen.so>
2016-04-08 10:53:47 +0200

    #1 added game component

---
+++ tdt4240-project/game.swift
@@ -0,0 +1,27 @@
+//
+//  game.swift
+//  tdt4240-project
+//
+//  Created by Kristoffer Larsen on 08.04.2016.
+//  Copyright © 2016 group-1. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+
+    static let instance = Game()
+    private var numPlayers = 1;
+    
+    func incrementPlayers() {
+        
+    }
+    
+    func decrementPlayers() {
+        
+    }
+    
+    func getPlayers () {
+        return numPlayers;
+    }
+}
