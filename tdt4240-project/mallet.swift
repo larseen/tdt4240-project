24e163807d22a36c1cd286692bc9aec17d4827bf
Andreas Ystmark <andreasystmark@dhcp-10-24-18-110.wlan.ntnu.no>
2016-04-11 10:13:47 +0200

    contact works

--- tdt4240-project/mallet.swift
+++ tdt4240-project/mallet.swift
@@ -1,28 +1,22 @@
-//
-//  puck.swift
-//  tdt4240-project
-//
-//  Created by Andreas Ystmark on 08/04/16.
-//  Copyright © 2016 group-1. All rights reserved.
-//
+
 
 import SpriteKit
 
 class Mallet: SKSpriteNode, SKPhysicsContactDelegate {
     
-    var mallet = SKSpriteNode(imageNamed: "Mallet")
+    //var mallet = SKSpriteNode(imageNamed: "Mallet")
     
     init() {
         let texture = SKTexture(imageNamed: "Mallet")
         super.init(texture: texture, color: SKColor.clearColor(), size: texture.size())
-        mallet.position = CGPointMake(frame.width/2, frame.height/4)
-        mallet.name = "mallet";
-        mallet.zPosition = 1
-        mallet.setScale(CGFloat(0.35))
-        mallet.physicsBody = SKPhysicsBody(circleOfRadius: mallet.size.width/2)
-        //mallet.physicsBody?.usesPreciseCollisionDetection = true
-        mallet.physicsBody?.categoryBitMask = CollisionCategories.malCol
-        mallet.physicsBody?.contactTestBitMask = CollisionCategories.puckCol
+        self.position = CGPointMake(frame.width/2, frame.height/4)
+        self.name = "mallet";
+        self.zPosition = 1
+        self.setScale(CGFloat(0.35))
+        self.physicsBody = SKPhysicsBody(circleOfRadius: self.size.width/2)
+        self.physicsBody?.usesPreciseCollisionDetection = true
+        self.physicsBody?.categoryBitMask = CollisionCategories.malCol
+        self.physicsBody?.contactTestBitMask = CollisionCategories.puckCol
     }
     
     required init?(coder aDecoder:NSCoder){
