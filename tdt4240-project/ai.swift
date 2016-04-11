8047808cf709865f021a6f12bf260370a8af9959
Kristoffer Larsen <kristoffer@larsen.so>
2016-04-11 11:38:57 +0200

    Update

---
+++ tdt4240-project/ai.swift
@@ -0,0 +1,33 @@
+//
+//  player.swift
+//  tdt4240-project
+//
+//  Created by Kristoffer Larsen on 11.04.2016.
+//  Copyright © 2016 group-1. All rights reserved.
+//
+
+
+import Foundation
+import SpriteKit
+
+class AI {
+    
+    /*
+     *
+     * PRIVATE VARIABLES
+     *
+     */
+    
+    /*
+     *
+     * RESET / INIT FUNCTIONS
+     *
+     */
+    
+    init() {
+
+    }
+    
+    
+    
+}
---
+++ tdt4240-project/player.swift
@@ -0,0 +1,47 @@
+//
+//  player.swift
+//  tdt4240-project
+//
+//  Created by Kristoffer Larsen on 11.04.2016.
+//  Copyright © 2016 group-1. All rights reserved.
+//
+
+
+import Foundation
+import SpriteKit
+
+class Player {
+    
+    /*
+     *
+     * PRIVATE VARIABLES
+     *
+     */
+    private var mallet : Mallet
+    private var id : Int
+    private var name : String
+    private var isAI : Bool
+    private var score : Int
+    
+    
+    /*
+     *
+     * RESET / INIT FUNCTIONS
+     *
+     */
+    
+    init(id : Int, name : String, isAI : Bool) {
+        self.id = id
+        self.mallet = Mallet()
+        self.name = name
+        self.score = 0
+        self.isAI = isAI || false
+    }
+    
+    
+    func getMallet() -> Mallet {
+        return mallet;
+    }
+    
+    
+}
