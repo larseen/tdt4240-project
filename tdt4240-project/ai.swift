f564e430ee9807e791a5a71421860e13f6f8013e
Ragnhild Neset <ragcecnes@gmail.com>
2016-04-21 11:11:26 +0200

    Add functionality to make ai move aroud

--- tdt4240-project/ai.swift
+++ tdt4240-project/ai.swift
@@ -11,23 +11,33 @@ import Foundation
 import SpriteKit
 
 class AI {
+    let speed = 200.0
+    var currentVelocity = CGVector(dx: 0, dy: 0)
     
-    /*
-     *
-     * PRIVATE VARIABLES
-     *
-     */
+    func newAction(player: Player,playerPos:CGPoint, puckPos: CGPoint)->SKAction{
+        if (puckPos.y > playerPos.y){
+            if (abs(puckPos.x - playerPos.x) > 50 ){
+                let direction = CGVector(dx: 0, dy: puckPos.y + 100 - playerPos.y)
+                let dist = sqrt(direction.dx*direction.dx + direction.dy*direction.dy)
+                currentVelocity = direction
+                return SKAction.moveTo(CGPoint(x: playerPos.x, y: puckPos.y + 100), duration: Double(dist)/speed)
+            }
+            return SKAction.moveByX(50,y:0, duration:50/speed)
+            
+        }
+        let dist = sqrt(pow(puckPos.x - playerPos.x, 2) + pow(puckPos.y - playerPos.y,2))
+        return SKAction.moveTo(CGPoint(x: puckPos.x, y: puckPos.y), duration: Double(dist)/speed)
+        
+    }
     
-    /*
-     *
-     * RESET / INIT FUNCTIONS
-     *
-     */
-    
-    init() {
-
+    func normalize(v: CGVector) -> CGVector{
+        let largest = max(v.dx, v.dy)
+        return CGVector(dx: v.dx/largest, dy: v.dx/largest)
+        
     }
     
     
+    
+
     
 }
--- tdt4240-project/game.scene.swift
+++ tdt4240-project/game.scene.swift
@@ -9,12 +9,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var playerOne : Player!
     var playerTwo : Player!
     var direction : CGVector!
+    var ai: AI!
     
     override func didMoveToView(view: SKView) {
         
         let physics:physicsFactory = physicsFactory();
         physicsWorld.contactDelegate = self
         physics.setPhys(self)
+        ai = AI()
         
         
         let game = Game.instance
@@ -24,13 +26,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         board = game.getBoard()
         puck = game.getPuck()
         if (game.getPlayers() == 2){
-            playerOne = Player(id: 1, name: "Player One", isAI: false, color: "blue", homeGoal: "top")
-            playerTwo = Player(id: 2, name: "Player Two", isAI: false, color: "red", homeGoal: "bottom")
+            playerOne = Player(id: 1, name: "Player One", isAI: false, color: "blue", homeGoal: "bottom")
+            playerTwo = Player(id: 2, name: "Player Two", isAI: false, color: "red", homeGoal: "top")
 
         }
         else {
-            playerOne = Player(id: 1, name: "Player One", isAI: false, color: "blue", homeGoal: "top")
-            playerTwo = Player(id: 2, name: "Player Two", isAI: true, color: "red", homeGoal: "bottom")
+            playerOne = Player(id: 1, name: "Player One", isAI: false, color: "blue", homeGoal: "bottom")
+            playerTwo = Player(id: 2, name: "Player Two", isAI: true, color: "red", homeGoal: "top")
             
         }
         
@@ -81,6 +83,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    override func update(currentTime: NSTimeInterval) {
+        if playerTwo.getIsAI() == 1{
+            let action = ai.newAction(playerTwo, playerPos: playerTwo.getMallet().position, puckPos: puck.position)
+            playerTwo.getMallet().runAction(action)
+        }
+        
+    }
+    
     
     func didBeginContact(contact: SKPhysicsContact){
         var firstBody: SKPhysicsBody
@@ -95,8 +105,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         if ((firstBody.categoryBitMask == CollisionCategories.puckCol) && (secondBody.categoryBitMask == CollisionCategories.malCol)){
-
-            firstBody.applyImpulse(direction)
+            firstBody.applyImpulse(CGVectorMake(5000*secondBody.velocity.dx, secondBody.velocity.dy*5000))
         
         }
         
