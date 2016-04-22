b512673093f253137bb6d86d993b4997e96ffeb6
elidakr <elida.reknes@gmail.com>
2016-04-22 13:00:03 +0200

    unfuck

--- tdt4240-project/highscorelistupdate.controller.swift
+++ tdt4240-project/highscorelistupdate.controller.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit
+
+class HighscoreListUpdateController : UIViewController, UITextFieldDelegate {
+    
+    let highscores : HighscoreList! = SinglePlayerHighscoreList()
+    var threshold: Int64 = 0
+    let score = 500//game.getScorePlayerOne();
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var noUpdateLabel: UILabel!
+    
+    func checkVisible(){
+        if (Int64(score) >= threshold){
+            self.noUpdateLabel.hidden = true
+        }
+        else {
+            self.nameLabel.hidden = true
+            self.submitButton.hidden = true
+            self.nameInput.hidden = true
+        }
+    }
+    
+    @IBAction func submitClicked(sender: AnyObject) {
+        highscores.updateHighscores(Highscore(score: Int64(self.score), name: nameInput.description))
+    }
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        self.threshold = highscores.getHighscoreThreshold()
+        
+        nameInput.delegate = self
+    }
+    
+    
+}
