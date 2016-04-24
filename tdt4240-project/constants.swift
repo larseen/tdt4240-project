7f92189d63b5c6aa8c746a46d71f51df0864b2b8
Anna Kastet <annakastet@gmail.com>
2016-04-24 15:21:52 +0200

    Add observer for game over
    Issue #48

--- tdt4240-project/constants.swift
+++ tdt4240-project/constants.swift
@@ -7,3 +7,6 @@ struct CollisionCategories{
     static let boardCol: UInt32 = 0x1 << 4
     static let powerupCol: UInt32 = 0x1 << 5
     }
+
+let GOALS_WHEN_GAME_OVER: Int = 7
+let GAME_OVER: String = "GAME_OVER" // Selector name for both player's score observer
