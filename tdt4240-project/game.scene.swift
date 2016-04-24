4114166cc7f2adf51505079e1d02453747825026
Andreas Ystmark <andreasystmark@dhcp-10-24-19-90.wlan.ntnu.no>
2016-04-24 12:50:37 +0200

    pls work

--- tdt4240-project/game.scene.swift
+++ tdt4240-project/game.scene.swift
@@ -2,31 +2,52 @@ import UIKit
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    weak var viewController: GameController!
+    var viewController: GameController!
     
     var board : Board!
     var puck: Puck!
     var playerOne : Player!
     var playerTwo : Player!
+    
+    var pauseButton : UIButton! = nil
+    var exitButton : UIButton! = nil
+    var storyBoard : UIStoryboard! = nil
+    var game : Game! = nil
+    
     var powerUpsHandler : PowerUpController?
+    
     
     override func didMoveToView(view: SKView) {
         powerUpsHandler = PowerUpController(gameScene: self)
-        let physics:physicsFactory = physicsFactory();
         physicsWorld.contactDelegate = self
-        physics.setPhys(self)
+        
+        
+        Helpers.setPhys(self)
+        
+        
+        
+        
+        
         let game = Game.instance
+        
         game.initGame(self.frame)
         
         board = game.getBoard()
         puck = game.getPuck()
         playerOne = game.getPlayerOne()
         playerTwo = game.getPlayerTwo()
-
+        
         self.addChild(board.board)
         self.addChild(puck)
         self.addChild(playerOne.getMallet())
         self.addChild(playerTwo.getMallet())
+        
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(GameScene.pause(_:)))
+        doubleTap.numberOfTapsRequired = 2
+        self.view?.addGestureRecognizer(doubleTap)
+        
+        
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)  {
@@ -44,6 +65,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         previousTimestamp = touch!.timestamp
         
         for touch in touches {
+            if (self.paused){
+                return
+            }
             let location = touch.locationInNode(self)
             if(nodeAtPoint(location).name == "mallet"){
                 let touchedNode = nodeAtPoint(location)
@@ -54,8 +78,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func update(currentTime: NSTimeInterval) {
+        
         if (playerTwo.isAi()){
             let action = playerTwo.getAi().newAction(playerTwo, playerPos: playerTwo.getMallet().position, puckPos: puck.position)
+            
             playerTwo.getMallet().runAction(action)
         }
         
@@ -97,13 +123,71 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             // Trying on some physics
             if ((playerTwo.isAi()) && secondBody.isEqual(playerTwo.getMallet().physicsBody)){
-        
+                
             } else{
                 puck.hit()
                 secondBody.velocity = CGVectorMake(0, 0)
             }
         }
     }
+    
+    
+    func pause(sender: UITapGestureRecognizer){
+        if (!self.paused){
+            self.paused = true;
+            pauseButton   = UIButton(type: UIButtonType.RoundedRect) as UIButton
+            pauseButton.frame = CGRectMake((self.frame.size.width/2) - 60, 140, 125, 50)
+            pauseButton.backgroundColor = UIColor.clearColor()
+            pauseButton.setTitle("Unpause", forState: UIControlState.Normal)
+            pauseButton.titleLabel?.font = UIFont(name: "helvetica", size: 25)
+            pauseButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+            exitButton   = UIButton(type: UIButtonType.RoundedRect) as UIButton
+            exitButton.frame = CGRectMake((self.frame.size.width/2) - 60, 200, 125, 50)
+            exitButton.backgroundColor = UIColor.clearColor()
+            exitButton.setTitle("Exit", forState: UIControlState.Normal)
+            exitButton.titleLabel?.font = UIFont(name: "helvetica", size: 25)
+            exitButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+            
+            
+            pauseButton.addTarget(self, action: #selector(GameScene.unpause(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+            exitButton.addTarget(self, action: #selector(GameScene.exitPressed(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+            
+            self.view!.addSubview(pauseButton)
+            self.view!.addSubview(exitButton)
+            
+        }
+        
+    }
+    
+    func exitPressed(sender:UIButton!){
+        
+        
+        
+        
+        puck.parent?.removeAllChildren()
+        
+        viewController = UIStoryboard(name: "main", bundle:nil).instantiateViewControllerWithIdentifier("ViewController") as! GameController
+        puck.parent?.removeAllChildren()
+        viewController.performSegueWithIdentifier("backMain", sender:self)
+        
+        
+        exitButton.removeFromSuperview()
+        pauseButton.removeFromSuperview()
+        
+        
+        
+        
+    }
+    
+    
+    func unpause(sender:UIButton!){
+        self.paused = false;
+        exitButton.removeFromSuperview()
+        pauseButton.removeFromSuperview()
+        
+    }
+    
+    
 }
 
 
